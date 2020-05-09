04e1a7c475ef0458bd461360225d1a824be9cf22
mattleahy <matthew.m.leahy@outlook.com>
2020-05-09 15:49:18 +0100

    add hook to render data on initial screen

---
+++ src/hooks/useRestaurants.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import yelp from "../api/yelp";
+
+export default () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setRestaurants(response.data.businesses);
+      setErrorMessage(null);
+    } catch (err) {
+      setRestaurants("");
+      setErrorMessage("Something went wrong!");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
+  return [searchApi, restaurants, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
-
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useRestaurants from "../hooks/useRestaurants";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [restaurants, setRestaurants] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 20,
-          term,
-          location: "san jose",
-        },
-      });
-      setRestaurants(response.data.businesses);
-      setErrorMessage(null);
-    } catch (err) {
-      setRestaurants("");
-      setErrorMessage("Something went wrong!");
-    }
-  };
+  const [searchApi, restaurants, errorMessage] = useRestaurants();
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {restaurants.length} restaurants</Text>
     </View>
