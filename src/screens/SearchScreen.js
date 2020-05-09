2434411e2525ffa2eef474eaa77901a215474b07
mattleahy <matthew.m.leahy@outlook.com>
2020-05-09 15:09:06 +0100

    add error handle to search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,22 +7,29 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [restaurants, setRestaurants] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 20,
-        term,
-        location: "san jose",
-      },
-    });
-    setRestaurants(response.data.businesses);
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 20,
+          term,
+          location: "san jose",
+        },
+      });
+      setRestaurants(response.data.businesses);
+      setErrorMessage(null);
+    } catch (err) {
+      setRestaurants("");
+      setErrorMessage("Something went wrong!");
+    }
   };
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {restaurants.length} restaurants</Text>
     </View>
   );
