84d9773db9a94d75b601843520e44b21532c6f84
mattleahy <matthew.m.leahy@outlook.com>
2020-05-10 12:59:18 +0100

    add information for no search results returned

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -12,7 +12,12 @@ import RestaurantsDetail from "./RestaurantsDetail";
 
 const RestaurantList = ({ title, restaurants, navigation }) => {
   if (!restaurants.length) {
-    return null;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.noRestaurants}>Sorry no restaurants found!</Text>
+      </View>
+    );
   }
   return (
     <View style={styles.container}>
@@ -47,6 +52,10 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginLeft: 15,
     marginBottom: 5,
+  },
+  noRestaurants: {
+    margin: 15,
+    justifyContent: "center",
   },
 });
 
